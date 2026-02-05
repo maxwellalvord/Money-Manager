ed5514cd31eaaa3a52daee278540a7c1eb36b91c
MA <maxwellalvord@gmail.com>
2026-02-05 12:10:15 -0800

    Next.js app startup, review layout, initial commit.

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,7 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Geist, Geist_Mono, Rubik} from "next/font/google";
 import "./globals.css";
 
-const geistSans = Geist({
+const rubik = Rubik({
   variable: "--font-geist-sans",
   subsets: ["latin"],
 });
@@ -20,7 +20,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${rubik.variable} ${geistMono.variable} antialiased`}
       >
         {children}
       </body>
