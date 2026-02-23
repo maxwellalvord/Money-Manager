99970c428ebf74591d54bbe5eb51f69572880550
MA <maxwellalvord@gmail.com>
2026-02-23 14:06:05 -0800

    use budgetlist function to gather user data relation to the expense id. Bugfix: params promise could not be reached, add react USE function to resolve

--- app/(routes)/Dash/_components/SideNav.jsx
+++ app/(routes)/Dash/_components/SideNav.jsx
@@ -38,9 +38,6 @@ function SideNav() {
     ]
     const path = usePathname();
 
-    useEffect(() => {
-        console.log(path)
-    }, [path])
     return (
         <div className='h-screen p-5 border shadow-sm'>
             <Image src='/logo.svg'
--- app/(routes)/Dash/expenses/[id]/page.jsx
+++ app/(routes)/Dash/expenses/[id]/page.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+"use client"
+import { db } from '@/utils/dbConfig'
+import { Budgets, Expenses } from '@/utils/schema'
+import { useUser } from '@clerk/nextjs'
+import { eq, getTableColumns, sql } from 'drizzle-orm'
+import React, { use, useEffect } from 'react'
 
-function Expenses(params) {
+function ExpensesScreen({params}) { 
+  const {id} = use(params)
+  const user = useUser();
+  useEffect(()=>{
+    user?.isSignedIn && getBudgetInfo();
+  },[user])
+  const getBudgetInfo = async() => {
+    const result = await db.select({
+          ...getTableColumns(Budgets),
+          totalSpend: sql `sum(${Expenses.amount})`.mapWith(Number),
+          totalItem: sql `count(${Expenses.id})`.mapWith(Number),
+        }).from(Budgets)
+        .leftJoin(Expenses, eq(Budgets.id, Expenses.budgetId))
+        .where(eq(Budgets.createdBy, user.primaryEmailAddress?.emailAddress))
+        .where(eq(Budgets.id, id))
+        .groupBy(Budgets.id)
+        
+        console.log(result);
+    
+  }
   return (
-    <div>Expenses</div>
+    <div className='p-8'>
+      <h2 className='text-3xl font-bold'>My Expenses</h2>
+    </div>
   )
 }
 
-export default Expenses
+export default ExpensesScreen
