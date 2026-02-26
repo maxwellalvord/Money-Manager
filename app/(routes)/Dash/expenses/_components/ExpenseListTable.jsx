1ef1ca724c00e554e8ec93e1250df92a68f43eea
MA <maxwellalvord@gmail.com>
2026-02-26 12:38:17 -0800

    add delete expense function to expense table, refresh data on click.

--- app/(routes)/Dash/expenses/_components/ExpenseListTable.jsx
+++ app/(routes)/Dash/expenses/_components/ExpenseListTable.jsx
@@ -1,7 +1,22 @@
+import { db } from '@/utils/dbConfig';
+import { Expenses } from '@/utils/schema';
+import { eq } from 'drizzle-orm';
 import { Trash } from 'lucide-react'
+import { refresh } from 'next/cache';
 import React from 'react'
+import { toast } from 'sonner';
 
-function ExpenseListTable({expensesList}) {
+function ExpenseListTable({expensesList, refreshData}) {
+
+    const deleteExpense = async(expense) => {
+        const res = await db.delete(Expenses).where(eq(Expenses.id, expense.id)).returning();
+
+        if(res)
+        {
+            toast('Expense Deleted Successfully!');
+            refreshData();
+        }
+    }
   return (
     <div className='mt-2'>
         <div className='grid grid-cols-4 bg-slate-200 p-2'>
@@ -20,7 +35,9 @@ function ExpenseListTable({expensesList}) {
                 <h2>{expenses.name}</h2>
                 <h2>${expenses.amount}</h2>
                 <h2>{date ? date.toDateString() : '-'}</h2>
-                <h2><Trash className='text-red-600'/></h2>
+                <h2><Trash className='text-red-600 cursor-pointer'
+                    onClick={() => deleteExpense(expenses)}
+                /></h2>
               </div>
             )
         })}
