765cfb6a47a223cb6ff4c7eb9b40de0bc298fd01
MA <maxwellalvord@gmail.com>
2026-04-27 15:37:15 -0700

    BugFix: add responsive container from rechart to allow barchart to be reponsive

--- app/(routes)/Dash/_components/BarChartDash.jsx
+++ app/(routes)/Dash/_components/BarChartDash.jsx
@@ -1,23 +1,25 @@
 import React from 'react'
-import { Bar, BarChart, Legend, Tooltip, XAxis, YAxis } from 'recharts'
+import { Bar, BarChart, Legend, ResponsiveContainer, Tooltip, XAxis, YAxis } from 'recharts'
 
 function BarChartDash({ budgetList }) {
   return (
-    <div className='border rounded-lg p-5'>
+    <div className='border rounded-lg p-5 w-full'>
       <h2 className='font-bold text-lg'> Budget Activity</h2>
-      <BarChart
-        width={500}
-        height={300}
-        data={budgetList}
-        margin={{top: 8}}
-      >
-        <XAxis dataKey="name" style={{ fontWeight: 400 }} />
-        <YAxis />
-        <Tooltip />
-        <Legend />
-        <Bar dataKey="totalSpend" stackId="A" fill="#25158F" />
-        <Bar dataKey="amount" stackId="A" fill="#64A8ED" />
-      </BarChart>
+      <div className='w-full h-[300px]'>
+        <ResponsiveContainer width='100%' height='100%'>
+          <BarChart
+            data={budgetList}
+            margin={{ top: 8, left: 0, right: 0, bottom: 0 }}
+          >
+            <XAxis dataKey="name" style={{ fontWeight: 400 }} />
+            <YAxis />
+            <Tooltip />
+            <Legend />
+            <Bar dataKey="totalSpend" stackId="A" fill="#25158F" />
+            <Bar dataKey="amount" stackId="A" fill="#64A8ED" />
+          </BarChart>
+        </ResponsiveContainer>
+      </div>
     </div>
   )
 }
