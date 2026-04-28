e261987585118918cf1b17b4961f81b7d1cc85be
MA <maxwellalvord@gmail.com>
2026-04-28 14:34:06 -0700

    Add button function to allow spamming the same expense if needed, otherwise can be toggled off to clear text box after submission.

--- app/(routes)/Dash/expenses/_components/AddExpense.jsx
+++ app/(routes)/Dash/expenses/_components/AddExpense.jsx
@@ -20,8 +20,13 @@ function AddExpense({budget, budgetId, user, refreshData}) {
             createdBy: user.primaryEmailAddress?.emailAddress,
             createdAt: new Date().toISOString()
         }).returning({insertedId:Budgets.id});
+
         if(res)
         {
+            if(clearOnSubmit === 1) {
+                setName('');
+                setAmount('');
+            }
             refreshData();
             toast('Expense Added Successfully')
         }
@@ -29,6 +34,7 @@ function AddExpense({budget, budgetId, user, refreshData}) {
     
     const [name, setName] = useState();
     const [amount, setAmount] = useState();
+    const [clearOnSubmit, setClearOnSubmit] = useState(0);
 
     // budget to expense comparision to see if we can submit new expense
     const numericAmount = Number(amount);
@@ -38,16 +44,21 @@ function AddExpense({budget, budgetId, user, refreshData}) {
 
   return (
     <div className= 'border p-5 rounded-lg'>
-        <h2 className= 'font-bold text-lg'>Add Expense</h2>
+        <div className='flex justify-between items-center mb-4'>
+            <h2 className= 'font-bold text-lg'>Add Expense</h2>
+            <Button variant='outline' onClick={() => setClearOnSubmit(clearOnSubmit ? 0 : 1)}>{clearOnSubmit ? 'Clear contents on submit: Enabled' : 'Clear contents on submit: Disabled'}</Button>
+        </div>
         <div className='mt-2'>
             <h2 className='text-black font-medium my-1'>Expense Name</h2>
             <Input placeholder='e.g. Record Store'
+            value={name || ''}
             onChange={(e) => setName(e.target.value)}
             />
         </div>
         <div className='mt-2'>
             <h2 className='text-black font-medium my-1'>Expense Amount</h2>
             <Input placeholder='e.g. 65'
+            value={amount || ''}
             onChange={(e) => setAmount(e.target.value)}
             />
             {remaining !== undefined && amount && !amountWithinBudget && (
