47cf18e2b945c2c747376c6a4ef130a00164645c
MA <maxwellalvord@gmail.com>
2026-04-28 13:44:11 -0700

    create Expenses page using list table component, had to fetch data and set page as client due to no location to send props through on root.

--- app/(routes)/Dash/expenses/page.jsx
+++ app/(routes)/Dash/expenses/page.jsx
@@ -1,8 +1,36 @@
-import React from 'react'
+"use client"
+
+import React, { useState, useEffect } from 'react'
+import ExpenseListTable from './_components/ExpenseListTable'
+import { db } from '@/utils/dbConfig'
+import { Expenses as ExpensesTable } from '@/utils/schema'
+import { eq } from 'drizzle-orm'
+import { useUser } from '@clerk/nextjs'
 
 function Expenses() {
+  const [expensesList, setExpensesList] = useState([])
+  const { user } = useUser()
+
+  useEffect(() => {
+    if (user) {
+      getExpensesList()
+    }
+  }, [user])
+
+  const getExpensesList = async () => {
+    const res = await db
+      .select()
+      .from(ExpensesTable)
+      .where(eq(ExpensesTable.createdBy, user?.primaryEmailAddress?.emailAddress))
+
+    setExpensesList(res)
+  }
+
   return (
-    <div>Expenses</div>
+    <div className='mt-5 ml-5'>
+      <h2 className='font-bold text-xl'>Check out your expenses below</h2>
+      <ExpenseListTable expensesList={expensesList} refreshData={getExpensesList}/>
+    </div>
   )
 }
 
